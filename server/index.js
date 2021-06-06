e43bd0e3eccdee9567145bf77e6a410731f67b63
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-06-06 16:38:41 +0530

    Made changes for deployment

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
