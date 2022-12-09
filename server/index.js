febb50b3ef036ceb03f6aa02abf78cb4a047683c
Ayush raj anand <rajanand.ayush98@gmail.com>
2022-12-09 16:18:57 +0530

    added vercel file

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-
+// middlewares
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
